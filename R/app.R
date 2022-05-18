c0730e1422c095cb972cbb801dedbce9ba754bf9
Tim Triche, Jr <tim.triche@gmail.com>
2022-05-18 12:14:55 -0400

    more fixes

--- R/app.R
+++ R/app.R
@@ -1,49 +1,55 @@
-invisible(sapply(list.files(
-  pattern = '(modules|^body|^sidebar|^condlPanel)',
-  recursive = TRUE,
-  full.names = TRUE),
-  function(f)
-    sys.source(f, env = attach(NULL))))
-
-ui <- dashboardPage(
-  skin = 'purple',
-  header = dashboardHeader(title = 'Simulations'),
-  sidebar = sidebarMenuOutput('tabs'),
-  body = body,
-  controlbar = dashboardControlbar(),
-  footer = dashboardFooter()
-)
-
-server <- function(input, output) {
+# dear god don't do this (below) sourcing all the files
+#
+if (FALSE) {
   
-  observeEvent(input$tabs, {
-    if(input$tabs == 'normal'){
-      callModule(normalServer, 'normalModule')
-    }
-    
-    if(input$tabs == 'bimod'){
-      callModule(bimodalServer, 'bimodalModule')
-    }
-    
-    if(input$tabs == 'multimod'){
-      callModule(multimodServer, 'multimodModule')
-    }
-    
-    if(input$tabs == 'unequalMode'){
-      callModule(unequalModeServer, 'unequalModeModule')
-    }
-    
-    if(input$tabs == 'unequalVar'){
-      callModule(unequalVarServer, 'unequalVarModule')
-    }
-    
-    if(input$tabs == 'samplesize'){
-      callModule(sampleSizeServer, 'sampleSizeModule')
-    }
-  })
-  
-  output$tabs <- renderMenu({ sidebar })
-  
-}
+  invisible(sapply(list.files(
+    pattern = '(modules|^body|^sidebar|^condlPanel)',
+    recursive = TRUE,
+    full.names = TRUE),
+    function(f)
+      sys.source(f, env = attach(NULL))))
 
-shinyApp(ui, server)
+  ui <- dashboardPage(
+    skin = 'purple',
+    header = dashboardHeader(title = 'Simulations'),
+    sidebar = sidebarMenuOutput('tabs'),
+    body = body,
+    controlbar = dashboardControlbar(),
+    footer = dashboardFooter()
+  )
+
+  server <- function(input, output) {
+    
+    observeEvent(input$tabs, {
+      if(input$tabs == 'normal'){
+        callModule(normalServer, 'normalModule')
+      }
+      
+      if(input$tabs == 'bimod'){
+        callModule(bimodalServer, 'bimodalModule')
+      }
+      
+      if(input$tabs == 'multimod'){
+        callModule(multimodServer, 'multimodModule')
+      }
+      
+      if(input$tabs == 'unequalMode'){
+        callModule(unequalModeServer, 'unequalModeModule')
+      }
+      
+      if(input$tabs == 'unequalVar'){
+        callModule(unequalVarServer, 'unequalVarModule')
+      }
+      
+      if(input$tabs == 'samplesize'){
+        callModule(sampleSizeServer, 'sampleSizeModule')
+      }
+    })
+    
+    output$tabs <- renderMenu({ sidebar })
+    
+  }
+
+  shinyApp(ui, server)
+
+} 
--- R/body.R
+++ R/body.R
@@ -1,42 +1,48 @@
-invisible(sapply(list.files(
-  pattern = '(modules|Module|^condlPanel)',
-  recursive = TRUE,
-  full.names = TRUE),
-  function(f)
-    sys.source(f, env = attach(NULL))))
+# also needs to be modularized so that it can be instantiated w/in the package
 
-body <- dashboardBody(
-  
-  tags$head(
-    tags$link(rel = 'stylesheet', type = 'text/css', href = '~/TR01/style.css')
-  ),
-  
-  tabItems(
-    tabItem(tabName = 'normal',
-            h2('Simulations under Normal Distribution'),
-            normalUI('normalModule')),
+if (FALSE) {
     
-    tabItem(tabName = 'unif',
-            h2('Simulations under Uniform Distribution')),
+  invisible(sapply(list.files(
+    pattern = '(modules|Module|^condlPanel)',
+    recursive = TRUE,
+    full.names = TRUE),
+    function(f)
+      sys.source(f, env = attach(NULL))))
+
+  body <- dashboardBody(
     
-    tabItem(tabName = 'bimod',
-            h2('Bimodal Instability'),
-            bimodalUI('bimodalModule')),
+    tags$head(
+      tags$link(rel = 'stylesheet', type = 'text/css', href = '~/TR01/style.css')
+    ),
     
-    tabItem(tabName = 'multimod',
-            h2('Multimodal: TBD'),
-            multimodUI('multimodModule')),
-    
-    tabItem(tabName = 'unequalMode',
-            h2('Power Testing for Unequal Mode Split'),
-            unequalModeUI('unequalModeModule')),
-    
-    tabItem(tabName = 'unequalVar',
-            h2('Unequal Variances: TBD'),
-            unequalVarUI('unequalVarModule')),
-    
-    tabItem(tabName = 'samplesize',
-            h2('Sample Size Calculations: TBD'),
-            sampleSizeUI('sampleSizeModule'))
-  )
-) # END dashboardBody
+    tabItems(
+      tabItem(tabName = 'normal',
+              h2('Simulations under Normal Distribution'),
+              normalUI('normalModule')),
+      
+      tabItem(tabName = 'unif',
+              h2('Simulations under Uniform Distribution')),
+      
+      tabItem(tabName = 'bimod',
+              h2('Bimodal Instability'),
+              bimodalUI('bimodalModule')),
+      
+      tabItem(tabName = 'multimod',
+              h2('Multimodal: TBD'),
+              multimodUI('multimodModule')),
+      
+      tabItem(tabName = 'unequalMode',
+              h2('Power Testing for Unequal Mode Split'),
+              unequalModeUI('unequalModeModule')),
+      
+      tabItem(tabName = 'unequalVar',
+              h2('Unequal Variances: TBD'),
+              unequalVarUI('unequalVarModule')),
+      
+      tabItem(tabName = 'samplesize',
+              h2('Sample Size Calculations: TBD'),
+              sampleSizeUI('sampleSizeModule'))
+    )
+  ) # END dashboardBody
+
+}
