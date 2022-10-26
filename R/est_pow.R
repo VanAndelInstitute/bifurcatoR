d9d1690e7ffe55b7e85d425a91b4fa992def1567
madajz <31075974+madajz@users.noreply.github.com>
2022-10-26 09:31:48 -0400

    Update est_pow.R

--- R/est_pow.R
+++ R/est_pow.R
@@ -36,17 +36,17 @@ est_pow = function(n,alpha,nsim,dist,params,tests){
       
     } else {
       if(dist=="weib"){
-    
-    n1 = floor(params$p*n)
-    n2 = floor((1-params$p)*n)
-    
-    n.dfs = lapply(1:nsim,function(x) c(rweibull(n1,shape=params$sp1,scale=params$sc1),rweibull(n2,shape=params$sp2,scale=params$sc2)))
-    
-    
-    a.dfs = lapply(1:nsim,function(x) c(rweibull(n,
-                                              shape = (n1 * params$sp1 + n2 * params$sp2)/n,
-                                              scale = (n1 * params$sc1 + n2 * params$sc2)/n)))
-    
+      print(params)
+      n1 = floor(params$p*n)
+      n2 = floor((1-params$p)*n)
+
+      n.dfs = lapply(1:nsim,function(x) c(rweibull(n1,shape=params$sp1,scale=params$sc1),rweibull(n2,shape=params$sp2,scale=params$sc2)))
+
+
+      a.dfs = lapply(1:nsim,function(x) c(rweibull(n,
+                                                shape = (n1 * params$sp1 + n2 * params$sp2)/n,
+                                                scale = (n1 * params$sc1 + n2 * params$sc2)/n)))
+
      }  
     }
   }
