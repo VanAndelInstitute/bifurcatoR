797bd8c26482b451c0990fce350afa877c6f8833
embertokarski <112200247+embertokarski@users.noreply.github.com>
2023-02-08 16:39:35 -0500

    update dxsmall file to limit genes and fusion groups.

--- inst/extdata/mRNA/dxsmall.reassemble.R
+++ inst/extdata/mRNA/dxsmall.reassemble.R
@@ -3,10 +3,14 @@ rowDat <- read.csv("dxsmall.rowData.csv", row=1)
 readcounts <- read.csv("dxsmall.readcounts.csv", row=1)
 lognormcounts <- read.csv("dxsmall.lognormcounts.csv", row=1)
 
+colDat <- colDat[colDat$FusionGroup != 'KDM5A',]
+rowDat <- rowDat[c('MECOM','PRDM16','CD34','KDM5D'),]
+readcounts <- readcounts[c('MECOM','PRDM16','CD34','KDM5D'), colDat$USI]
+lognormcounts <-lognormcounts[c('MECOM','PRDM16','CD34','KDM5D'), colDat$USI]
+
 library(SummarizedExperiment) 
 dxsmall <- SummarizedExperiment(rowData=rowDat, 
                                 colData=colDat, 
                                 assays=list(counts=readcounts, 
                                             logcounts=lognormcounts))
-save(dxsmall, file="../../data/dxsmall.rda", compress="xz")
-
+save(dxsmall, file="../../../data/dxsmall.rda", compress="xz")
