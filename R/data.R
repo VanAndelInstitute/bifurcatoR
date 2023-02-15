38127eb54e4f18948099a91f1f110c64393f0b14
Tim Triche, Jr <tim.triche@gmail.com>
2023-02-15 13:32:28 -0500

    fix iSEE issue and refactor out iSEEapp() function

--- R/data.R
+++ R/data.R
@@ -11,37 +11,15 @@
 #' @examples
 #'
 #'  data(dxsmall)
-#'  dxsmall$MLLrearranged <- dxsmall$FusionGroup == "MLL"
-#'  if(requireNamespace("iSEE")) {
 #' 
-#'    library(SingleCellExperiment)
-#'    se <- as(dxsmall, "SingleCellExperiment") 
-#'
-#'    library(uwot) # for the reduced dimension plot
-#'    reducedDim(se, "UMAP") <- umap(t(logcounts(se)), metric="cosine")
-#'
-#'    library(iSEE)
-#'    app <- iSEE(se,
-#'                initial=list(
-#'                  UMAP=new("ReducedDimensionPlot",
-#'                           ColorByColumnData = "MLLrearranged",
-#'                           ColorBy = "Column data", 
-#'                           Type = "UMAP"),
-#'                  Gene=new("RowDataTable"),
-#'                  Expression=new("FeatureAssayPlot", 
-#'                                 Assay = "logcounts", 
-#'                                 XAxis = "Column data", 
-#'                                 XAxisColumnData = "MLLrearranged", 
-#'                                 YAxisFeatureSource = "Gene", 
-#'                                 YAxisFeatureDynamicSource = TRUE, 
-#'                                 ColorByColumnData = "MLLrearranged", 
-#'                                 ColorBy = "Column data") 
-#'                )
-#'    )
-#'
-#'    # finally, run the damn thing:
-#'    shiny::runApp(app, port=12345)
-#'
-#'  }
+#'  # if `iSEE` is installed:
+#'  show(dxsmall)
+#'  rownames(dxsmall)
+#'  names(colData(dxsmall))
+#'  table(dxsmall$FusionGroup)
+#'  table(dxsmall$AgeGroup)
+#' 
+#'  # if `iSEE` is installed:
+#'  iSEEapp(dxsmall)
 #' 
 "dxsmall"
