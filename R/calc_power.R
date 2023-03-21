b543d9ea3edf79ab6379568162a367b8e2ff322e
mao-ding <mding.mhir@gmail.com>
2023-03-21 14:25:08 -0400

    test on R local on mao-ding's branch by Mao

--- R/calc_power.R
+++ R/calc_power.R
@@ -63,5 +63,5 @@ calc_power = function(n, p1, p2, shift, sel, CI, alpha=0.05, nsim=100) {
   ))
   
 }
-
+###################################################
 ### test on R local on mao-ding's branch by Mao ###
