db3c35af56521c08d8a6702de8f0f9646e6ead1b
Tim Triche, Jr <tim.triche@gmail.com>
2024-03-20 17:03:59 -0400

    document data(MLL) properly (#33)

--- R/data.R
+++ R/data.R
@@ -1,3 +1,26 @@
+#' MLL
+#'
+#' Data from MLL-rearranged pediatric AML patients in COG clinical trials.
+#' AgeGroup, Sex, FAB, BlastPercent, fusion, OS, OSI, and Protocol are clinical.
+#' MECOM, PRDM16, CD33, CD34, NCAM1, and KDM5D are log-normalized mRNA CPMs 
+#' (counts per million reads) for six genes, some of which are informative about
+#' clinical outcomes (see vignette for more details). 
+#'
+#' @importFrom utils data
+#' 
+#' @usage data(MLL)
+#' @format A \code{data.frame}
+#'
+#' @examples
+#'
+#'  data(MLL)
+#'  library(mixR) 
+#'  MLLfit <- mixfit(MLL$MECOM, ncomp=2)
+#'  plot(MLLfit, xlab="log(MECOM transcripts)")
+#' 
+"MLL"
+
+
 #' dxsmall
 #'
 #' A SummarizedExperiment with covariates and mRNA counts from 501 AML cases.
