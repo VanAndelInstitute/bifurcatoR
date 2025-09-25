1df2a95203dff279eefa661bfa018a64e7fba837
James Eapen <james.eapen@vai.org>
2025-09-25 18:49:20 -0400

    fix(imports): add olsrr::ols_test_f

--- R/est_pow_het.R
+++ R/est_pow_het.R
@@ -12,6 +12,6 @@
 #' 
 #' @export
 est_pow_het = function(n,p,alpha,x,nsim){
-  power = sum(sapply(1:nsim,function(y) as.numeric((I(ols_test_f(lm(val~g,data.frame(val = c(rnorm(round(n*p),0,1),rnorm(round(n*abs((1-p))),0,x)),g=c(rep("x",round(n*p)),rep("y",round(n*abs(1-p)))))))$p<alpha)))))/nsim
+  power = sum(sapply(1:nsim,function(y) as.numeric((I(olsrr::ols_test_f(lm(val~g,data.frame(val = c(rnorm(round(n*p),0,1),rnorm(round(n*abs((1-p))),0,x)),g=c(rep("x",round(n*p)),rep("y",round(n*abs(1-p)))))))$p<alpha)))))/nsim
   return(round(power,4))
 }
