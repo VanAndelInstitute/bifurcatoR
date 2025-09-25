f2b831d850653011a1b93fd53ffddac079df32be
James Eapen <james.eapen@vai.org>
2025-09-25 16:44:40 -0400

    fix(imports): don't import mixR, all uses namespaced

--- R/bs_lrt.R
+++ R/bs_lrt.R
@@ -23,7 +23,6 @@
 #' @seealso mixR::bs.test
 #' @seealso mixR
 #'
-#' @importFrom mixR bs.test
 #' @import ppclust
 #'
 #' @export
@@ -79,11 +78,11 @@ bs_lrt <- function(x, H0=1, H1=2, family="normal", nboot=1e2, iter=1e3, ...){
   if(!is.numeric(H1) || H1 < H0) stop("H1 must be an integer greater than H0.")
 
   # perform the bootstrap test
-  tst.p = bs.test(x, ncomp=c(H0, H1), family=family, B=nboot, max_iter=iter, ...)
+  tst.p = mixR::bs.test(x, ncomp=c(H0, H1), family=family, B=nboot, max_iter=iter, ...)
   # If this test comes back NA, it usually means the EM algorithm failed to converge on two unequal modes.
   # Attempt with equal variances to get a p-value. An alternate option would be to set NA -> p =0.99999
   if(is.na(tst.p$pvalue)){
-    tst.p = bs.test(x, ncomp=c(H0, H1), family=family, B=nboot, max_iter=iter,ev=T ,...)
+    tst.p = mixR::bs.test(x, ncomp=c(H0, H1), family=family, B=nboot, max_iter=iter,ev=T ,...)
   }
   
   return(tst.p)
