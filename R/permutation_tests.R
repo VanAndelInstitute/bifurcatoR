0c9052564d7b21614ed37e5667b717b7bbceb91d
Zachary Madaj <zachary.madaj@vai.org>
2026-02-24 14:39:00 -0500

    Refactoring of the permutation test function to make it more efficient.

--- R/permutation_tests.R
+++ R/permutation_tests.R
@@ -10,19 +10,7 @@
 #' @importFrom stats ecdf quantile
 #'
 #' @export
-permutation_tests = function(temp_df,nperm,fxn,alpha) {
-  # Get actual absolute mean difference.
-  actual_diff <- do.call(fxn, list(temp_df$y, temp_df$X))
-  # Find shuffled mean differences.
-  shuffled_diff <- sapply(1:nperm, function(p){
-    shuffled_idx <- sample(1:nrow(temp_df))
-    shuffled_X <- temp_df$X[shuffled_idx]
-    return(do.call(fxn,list(temp_df$y, shuffled_X)))
-  })
 
-  percentile <- ecdf(shuffled_diff)
-  p = 1 - percentile(actual_diff)
-  return(list(p = p,diff = actual_diff, crit = quantile(shuffled_diff,p=c(alpha/2,(1-alpha/2)))))
 permutation_tests <- function(y, X, nperm, fxn_names, alpha) {
   stopifnot(length(y) == length(X))
   stopifnot(is.numeric(nperm), nperm > 0)
